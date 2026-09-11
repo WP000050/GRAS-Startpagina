150811310968e678346cb7b6c63512aa2f457614
WP000050 <p000050@ozcsvorselaar.be>
2026-09-11 16:56:31 +0200

    Change toolbox links to vbsgrasheide.be
    Updated toolbox links to new site URLs.

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ const LEERLING_DOMEIN = "leerling.vbsgrasheide.be";
 // 2. TOOLBOXLINKS
 // Vul tussen de aanhalingstekens "" de gewenste URL in voor elke klas.
 const TOOLBOX_LINKS = {
-  l1:  "https://voorbeeld.be/toolbox-l1",
-  l2:  "https://voorbeeld.be/toolbox-l2",
-  l3:  "https://voorbeeld.be/toolbox-l3",
-  l4:  "https://dolphin-2gvz1d.my.canva.site",
-  l5:  "https://voorbeeld.be/toolbox-l5",
-  l6:  "https://voorbeeld.be/toolbox-l6",
-  "4J": "https://voorbeeld.be/toolbox-4j",
-  "5J": "https://voorbeeld.be/toolbox-5j"
+  l1:  "https://site.vbsgrasheide.be/",
+  l2:  "https://site.vbsgrasheide.be/",
+  l3:  "https://site.vbsgrasheide.be/",
+  l4:  "https://dolphin-2gvz1d.my.canva.site/",
+  l5:  "https://site.vbsgrasheide.be/",
+  l6:  "https://site.vbsgrasheide.be/",
+  "4J": "https://site.vbsgrasheide.be/",
+  "5J": "https://site.vbsgrasheide.be/"
 };
